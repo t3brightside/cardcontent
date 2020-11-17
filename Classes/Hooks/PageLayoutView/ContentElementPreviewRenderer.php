14e70902fb1c3d770e24ad2e0794d763db267226
Tanel Põld <tanel@rightside.ee>
2020-11-17 11:04:07 +0200

    Add bodytext preview to BE

--- Classes/Hooks/PageLayoutView/ContentElementPreviewRenderer.php
+++ Classes/Hooks/PageLayoutView/ContentElementPreviewRenderer.php
@@ -28,6 +28,7 @@ class ContentElementPreviewRenderer implements PageLayoutViewDrawItemHookInterfa
                     unset($linkedContent);
                 }
             }
+			$itemContent .= $parentObject->linkEditContent($parentObject->renderText($row['bodytext']), $row);
 			$itemContent .= '<ul style="margin: 0; margin-top: 0.5em; padding: 0.2em 1.4em;">';
     		$itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Template: ' . $row['tx_cardcontent_template']), $row) . '</li>';
 			$itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Image crop: ' . $row['tx_cardcontent_cropratio']), $row) . '</li>';
