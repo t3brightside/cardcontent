38738b26ee94b250a04a800d5eb3f87cbe5d0965
Tanel <tanel@brightside.ee>
2020-12-08 20:27:43 +0200

    Enable longer links

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tt_content (
     tx_cardcontent_template varchar(25),
     tx_cardcontent_cropratio varchar(25),
-    tx_cardcontent_link tinytext,
+    tx_cardcontent_link varchar(1000),
     tx_cardcontent_linktext tinytext,
 );
 
